34af83318e410ac54651d780f8f142433df7f3ba
Manikanta Pillimetla <127874127+ManikantaGit2@users.noreply.github.com>
2025-01-07 23:25:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+
+const tabs = document.querySelectorAll(".tabs h3");
+const tabContents = document.querySelectorAll(".tab-content div");
+
+tabs.forEach((tab, index)=>{
+    tab.addEventListener("click",()=>{
+    tabContents.forEach((content)=>{
+        content.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+        tab.classList.remove("active");
+    });
+    tabContents[index].classList.add("active");
+    tabs[index].classList.add("active");
+    });
+});
